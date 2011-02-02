37356f97ffbfe04e7086c021d98426e38a3aac5c
Eric Boehs <ericboehs@gmail.com>
2011-02-01 18:11:30 -0600

    Adding spec, gemfile.lock, and tweeked accents

--- lib/to_slug.rb
+++ lib/to_slug.rb
@@ -1,49 +1,52 @@
-module ToSlug
+# coding: utf-8
 
-  # coding: utf-8
-  class String # This reopns the string class
-
-    # Define a new method to convert the string to be url friendly
-    def to_slug
-
-      # This is the input as there are no arguments passed.
-      string = self
-
-      # Define which accents map to which ascii characters
-      accents = {
-        ['á','à','â','ä','ã'] => 'a',
-        ['Ã','Ä','Â','À','�?'] => 'A',
-        ['é','è','ê','ë'] => 'e',
-        ['Ë','É','È','Ê'] => 'E',
-        ['í','ì','î','ï'] => 'i',
-        ['�?','Î','Ì','�?'] => 'I',
-        ['ó','ò','ô','ö','õ'] => 'o',
-        ['Õ','Ö','Ô','Ò','Ó'] => 'O',
-        ['ú','ù','û','ü'] => 'u',
-        ['Ú','Û','Ù','Ü'] => 'U',
-        ['ç'] => 'c', ['Ç'] => 'C',
-        ['ñ'] => 'n', ['Ñ'] => 'N'
-      }
-
-      # Replace each accent in the string
-      accents.each do |accent, replacement|
-        accent.each do |character|
-          string = string.gsub(character, replacement)
-        end
+class String # This reopns the string class
+  # Define a new method to convert the string to be url friendly
+  def to_slug
+    # This is the input as there are no arguments passed.
+    string = self
+    
+    # Define which accents map to which ascii characters
+    accents = {
+      ['á','à','â','ä','ã','å'] => 'a',
+      ['Ã','Ä','Â','À','Ã'] => 'A',
+      ['é','è','ê','ë'] => 'e',
+      ['Ë','É','È','Ê'] => 'E',
+      ['í','ì','î','ï'] => 'i',
+      ['�?','Î','Ì','�?'] => 'I',
+      ['ó','ò','ô','ö','õ'] => 'o',
+      ['Õ','Ö','Ô','Ò','Ó'] => 'O',
+      ['ú','ù','û','ü','ů'] => 'u',
+      ['Ú','Û','Ù','Ü'] => 'U',
+      ['ç'] => 'c', ['Ç'] => 'C',
+      ['ñ'] => 'n', ['Ñ'] => 'N',
+      ['ý'] => 'y'
+    }
+    
+    # Replace each accent in the string
+    accents.each do |accent, replacement|
+      accent.each do |character|
+        string = string.gsub(character, replacement)
       end
-
-      # Remove any characters that aren't alphanumeric
-      string = string.gsub(/[^a-zA-Z0-9 ]/,"")
-
-      # Convert multiple spaces to a single space
-      string = string.gsub(/[ ]+/," ")
-
-      # Replace single spaces with a URL friendly dash (-)
-      string = string.gsub(/ /,"-")
-
-      # CASE. DOWN. EVERYTHING. (and return since it's the last line)
-      string = string.downcase
-
     end
+    
+    # Convert underscores to dashs
+    string = string.gsub(/[_]/,"-")
+    
+    # Remove any characters that aren't alphanumeric (or a dash)
+    string = string.gsub(/[^a-zA-Z0-9 \-]/,"")
+    
+    # Convert multiple spaces to a single space
+    string = string.gsub(/[ ]+/," ")
+    
+    # Give a little trim around the edges (leading/trailing whitespace)
+    string.strip!
+    
+    # Replace single spaces with a URL friendly dash (-)
+    string = string.gsub(/ /,"-")
+    
+    # CASE. EVERYTHING. DOWN. (and return since it's the last line)
+    string = string.downcase
+    
   end
 end
---
+++ spec/to_slug_spec.rb
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+require 'to_slug'
+
+describe String, "to_slug" do
+
+  it "replaces spaces with dashes" do
+    "This is a string".to_slug == "this-is-a-string"
+  end
+
+  it "converts accented characters to a close ASCII alternative" do
+    "aëiòú".to_slug.should == "aeiou"
+  end
+
+  it "converts lots of accented characters to a close ASCII alternative" do
+    "thîs ís a nåsty strïng éëê. ýůçký".to_slug.should == "this-is-a-nasty-string-eee-yucky"
+  end
+
+  it "converts a single accented character to a close ACII alternative" do
+    "å".to_slug.should == "a"
+  end
+
+  it "converts scores to dashes" do
+    "This is an example to_slug".to_slug.should == "this-is-an-example-to-slug"
+  end
+
+  it "removes leading/trailing whitespace" do
+    " this is a string ".to_slug.should == "this-is-a-string"
+  end
+
+  it "removes leading punctuation" do
+    "!!*% This is leading to somewhere cool".to_slug.should == "this-is-leading-to-somewhere-cool"
+  end
+
+  it "removes single quotes" do
+    "This is a 'quoted' string".to_slug.should == "this-is-a-quoted-string"
+  end
+
+  it "removes double quotes" do
+    'This is a "quoted" string'.to_slug.should == "this-is-a-quoted-string"
+  end
+
+  it "handles multiple spaces" do
+    "I like to put extra spaces.  See".to_slug.should == "i-like-to-put-extra-spaces-see"
+  end
+
+  it "preserves dashes" do
+    "I like Mrs - on my salad".to_slug.should == "i-like-mrs---on-my-salad"
+  end
+
+  it "downcases all characters" do
+    "THIS IS SPARTAAAaaa".to_slug.should == "this-is-spartaaaaaa"
+  end
+
+  it "accepts empty strings" do
+    "".to_slug.should == ""
+  end
+
+  it "remains a String class" do
+    "I am a string class yo!".to_slug.should be_kind_of(String)
+  end
+
+end
